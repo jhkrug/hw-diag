74e0bdb561d0bbf35c642d10080c3901b58210bd
John Krug <john.krug@suse.com>
2023-11-06 17:47:43 +0100

    githublinks?
    Signed-off-by: John Krug <john.krug@suse.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,7 +43,7 @@ const config = {
           sidebarCollapsed: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/jhkrug/hw-diag/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/jhkrug/hw-diag/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
